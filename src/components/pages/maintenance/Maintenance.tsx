f1d6dd6dd9187f9d1bb1f7fdf762c47ac3a7c7b0
Jordy Cruz Suasnabar <jordycs93@gmail.com>
2024-12-04 20:54:19 -0500

    update: se corrige vista de mantenimiento key prop.

--- src/components/pages/maintenance/Maintenance.tsx
+++ src/components/pages/maintenance/Maintenance.tsx
@@ -3,24 +3,25 @@ import Navbar from "../../ui/navbar/Navbar";
 import Footer from "../../ui/footer/Footer";
 
 interface Opcion {
+  index: number;
   titulo: string;
   icono: JSX.Element;
   ruta: string;
 }
 
 const opciones: Opcion[] = [
-  { titulo: "Tercero", icono: <span className="icon">🏢</span>, ruta: '/tercero' },
-  { titulo: "Sede", icono: <span className="icon">📍</span>, ruta: '/sede' },
-  { titulo: "Residuo", icono: <span className="icon">♻️</span>, ruta: '/residuo' },
-  { titulo: "Estado", icono: <span className="icon">📊</span>, ruta: '/estado' },
-  { titulo: "Vehículo", icono: <span className="icon">🚚</span>, ruta: '/vehiculo' },
-  { titulo: "Conductor", icono: <span className="icon">👤</span>, ruta: '/conductor' },
-  { titulo: "Usuario", icono: <span className="icon">🧑</span>, ruta: '/usuario' },
-  { titulo: "Relleno Sanitario", icono: <span className="icon">⛰️</span>, ruta: '/relojo' },
-  { titulo: "Método de Pago", icono: <span className="icon">💳</span>, ruta: '/metodo' },
-  { titulo: "Impuesto", icono: <span className="icon">🧾</span>, ruta: '/impuesto' },
-  { titulo: "Coordinador", icono: <span className="icon">💼</span>, ruta: '/coordinador' },
-  { titulo: "Empresa Operadora", icono: <span className="icon">🏭</span>, ruta: '/empresa' },
+  { index: 1, titulo: "Tercero", icono: <span className="icon">🏢</span>, ruta: '/tercero' },
+  { index: 2, titulo: "Sede", icono: <span className="icon">📍</span>, ruta: '/sede' },
+  { index: 3, titulo: "Residuo", icono: <span className="icon">♻️</span>, ruta: '/residuo' },
+  { index: 4, titulo: "Estado", icono: <span className="icon">📊</span>, ruta: '/estado' },
+  { index: 5, titulo: "Vehículo", icono: <span className="icon">🚚</span>, ruta: '/vehiculo' },
+  { index: 6, titulo: "Conductor", icono: <span className="icon">👤</span>, ruta: '/conductor' },
+  { index: 7, titulo: "Usuario", icono: <span className="icon">🧑</span>, ruta: '/usuario' },
+  { index: 8, titulo: "Relleno Sanitario", icono: <span className="icon">⛰️</span>, ruta: '/relojo' },
+  { index: 9, titulo: "Método de Pago", icono: <span className="icon">💳</span>, ruta: '/metodo' },
+  { index: 10, titulo: "Impuesto", icono: <span className="icon">🧾</span>, ruta: '/impuesto' },
+  { index: 11, titulo: "Coordinador", icono: <span className="icon">💼</span>, ruta: '/coordinador' },
+  { index: 12, titulo: "Empresa Operadora", icono: <span className="icon">🏭</span>, ruta: '/empresa' },
 ];
 
 export default function Maintenance() {
@@ -31,9 +32,9 @@ export default function Maintenance() {
     <h1 className="heading">Mantenimiento</h1>
 
       <div className="gridM">
-        {opciones.map((opcion, index) => (
-          <a href={opcion.ruta}>
-            <div className="cardM" key={index}>
+        {opciones.map((opcion) => (
+          <a href={opcion.ruta}  key={opcion.index}>
+            <div className="cardM">
             <div className="cardM-content">
               {opcion.icono}
               <h2 className="cardM-title">{opcion.titulo}</h2>
